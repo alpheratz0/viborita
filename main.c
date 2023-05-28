113304d2cdb1189a04589429dde97ccf663ea374
alpheratz0 <alpheratz99@protonmail.com>
2023-05-28 03:46:16 -0300

    test map_stringify

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "map.h"
+#include <stdio.h>
 
 char *app_name = "main.c";
 
@@ -6,8 +7,11 @@ int
 main(void)
 {
 	struct map default_map;
+	char map_str[2949];
 
 	map_parse_file(&default_map, "./maps/default");
+	map_stringify(&default_map, sizeof(map_str), map_str);
+	printf("%s", map_str);
 
 	return 0;
 }
