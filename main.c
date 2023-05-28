c774ec9bac320b4c893919c5ff2db3d4fba1185e
alpheratz0 <alpheratz99@protonmail.com>
2023-05-28 19:59:30 -0300

    basic test for map_advance fn

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "map.h"
 #include <stdio.h>
+#include <unistd.h>
 
 char *app_name = "main.c";
 
@@ -7,11 +8,19 @@ int
 main(void)
 {
 	struct map default_map;
+	enum map_viborita_state viborita_state;
 	char map_str[2949];
 
 	map_parse_file(&default_map, "./maps/default");
-	map_stringify(&default_map, sizeof(map_str), map_str);
-	printf("%s", map_str);
+
+	do
+	{
+		map_advance(&default_map, &viborita_state);
+		map_stringify(&default_map, sizeof(map_str), map_str);
+		printf("%s", map_str);
+		sleep(1);
+	}
+	while (viborita_state != MAP_VIBORITA_DEAD);
 
 	return 0;
 }
