803a2eab9c950b78589355a1b9e485dc73c81261
alpheratz0 <alpheratz99@protonmail.com>
2023-05-31 20:37:31 -0300

    simplify ncurses version

--- main_ncurses.c
+++ main_ncurses.c
@@ -4,51 +4,50 @@
 #include <ncurses.h>
 
 int
-main(void)
+main(int argc, char **argv)
 {
-	struct map default_map;
-	struct map pmap; // Playing map.
-	enum map_viborita_state viborita_state;
+	int score = 0;
+	struct map map;
+	enum map_viborita_state state;
 	char map_str[2949];
 	int c;
 
-	if (map_parse_file(&default_map, "./maps/default") < 0)
+	if (argc < 2 || map_parse_file(&map, argv[1]) < 0)
 		return 1;
 
 	initscr();
 	nodelay(stdscr, TRUE);
 	noecho();
 
-RESET:
-	map_copy(&default_map, &pmap);
-
 	do
 	{
-		usleep(100000);
-
 		while ((c = getch()) != ERR) switch (c)
 		{
-			case 'h': map_set_viborita_direction(&pmap, MAP_BLOCK_VIBORITA_LEFT); break;
-			case 'j': map_set_viborita_direction(&pmap, MAP_BLOCK_VIBORITA_DOWN); break;
-			case 'k': map_set_viborita_direction(&pmap, MAP_BLOCK_VIBORITA_UP); break;
-			case 'l': map_set_viborita_direction(&pmap, MAP_BLOCK_VIBORITA_RIGHT); break;
+			case 'h': map_set_viborita_direction(&map, MAP_BLOCK_VIBORITA_LEFT); break;
+			case 'j': map_set_viborita_direction(&map, MAP_BLOCK_VIBORITA_DOWN); break;
+			case 'k': map_set_viborita_direction(&map, MAP_BLOCK_VIBORITA_UP); break;
+			case 'l': map_set_viborita_direction(&map, MAP_BLOCK_VIBORITA_RIGHT); break;
 		}
 
-		map_advance(&pmap, &viborita_state);
+		map_advance(&map, &state);
 
-		if (viborita_state == MAP_VIBORITA_EATING)
-			map_spawn_food(&pmap);
+		if (state == MAP_VIBORITA_EATING)
+		{
+			map_spawn_food(&map);
+			score += 1;
+		}
 
-		map_stringify(&pmap, sizeof(map_str), map_str);
+		map_stringify(&map, sizeof(map_str), map_str);
 		move(0, 0);
 		printw(map_str);
 		refresh();
-	}
-	while (viborita_state != MAP_VIBORITA_DEAD);
 
-	goto RESET;
+		usleep(100000);
+	}
+	while (state != MAP_VIBORITA_DEAD);
 
 	endwin();
+	printf("xviborita score: %d\n", score);
 
 	return 0;
 }
